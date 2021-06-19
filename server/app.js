eb92deb0d6a57d3c08d1c7f12655ea2711083ef3
fjjes <fjjesreen@gmail.com>
2021-06-19 17:53:07 -0600

    setup server to work with react Router routes

--- server/app.js
+++ server/app.js
@@ -29,6 +29,10 @@ app.use('/api', apiRouter)
 // serve the react application
 app.use(express.static('../client/build'))
 
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.resolve(__dirname,'../client/build', 'index.html'))
+})
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- server/models/db.js
+++ server/models/db.js
@@ -10,15 +10,15 @@ const mongoose = require('mongoose');
 //   `mongodb+srv://${mongoUser}:${mongoPasswd}` +
 //   `@${mongoServer}/${mongoDBName}?retryWrites=true&w=majority`;
 
-const localMongoUrl = "mongodb://localhost:27017/c6Superheroes"
+//const localMongoUrl = "mongodb://localhost:27017/c6Superheroes"
 
-//mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(localMongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(localMongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.once('open', (_) =>
-  console.log('MongoDB is now connected:', localMongoUrl)
-  //console.log('MongoDB is now connected:', process.env.MONGODB_URL)
+  //console.log('MongoDB is now connected:', localMongoUrl)
+  console.log('MongoDB is now connected:', process.env.MONGODB_URL)
 
 );
 db.on('error', (err) => console.error('MongoDB connection error!', err));
